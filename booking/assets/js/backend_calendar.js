2f998cc5505182b8170829ac9ceac188483f4630
Dunatotatos <contact@dunatotatos.com>
2016-10-07 18:32:39 +0300

    Cean previous commit

--- booking/assets/js/backend_calendar.js
+++ booking/assets/js/backend_calendar.js
@@ -1540,7 +1540,6 @@ var BackendCalendar = {
      * a data preview popover is display above the calendar item.
      */
     calendarEventClick: function(event, jsEvent, view) {
-        console.log(event);
         $('.popover').remove(); // Close all open popovers.
 
         var html, displayEdit, displayDelete;
@@ -1609,7 +1608,7 @@ var BackendCalendar = {
                         + event.data['customer']['first_name'] + ' '
                         + event.data['customer']['last_name']
                         + '<br>' +
-                    '<strong>' + EALang['nb_participants'] + '</strong> '
+                    '<strong>' + EALang['nb_persons'] + '</strong> '
                         + event.data['nb_persons']
                         + '<hr>' +
                     '<center>' +
