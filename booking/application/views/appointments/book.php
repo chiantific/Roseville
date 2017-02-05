57bce08451ed018625bb385594379630e06f9960
Arnaud Hungler <arnaud.hungler@kaust.edu.sa>
2017-02-05 14:15:42 +0300

    Localize page title

--- booking/application/views/appointments/book.php
+++ booking/application/views/appointments/book.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#35A768">
-    <title>Réservation</title>
+    <title><?php echo $this->lang->line('page_title'); ?></title>
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 
     <link
