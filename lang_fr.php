69ed76e776e5735bdc4df576c4df86c2ee01ba8b
U-KAUST\fiumelh <fiumelh@PC-KL-15332.KAUST.EDU.SA>
2017-02-26 00:03:39 +0300

    change the meta-description text

--- lang_fr.php
+++ lang_fr.php
@@ -1,7 +1,7 @@
 <?php
 // french
 $lang['main_title'] = 'Roseville Escape Room';
-$lang['meta_description_content'] = 'Roseville Escape est un jeu d\'évasion situé à deux pas de Vevey. Une activité originale dans un décor insolite faisant appel à l\'esprit d\'équipe pour s\'amuser en famille, entre amis ou collègues.';
+$lang['meta_description_content'] = 'Escape Game à Vevey vous immerge dans des décors époustouflants! Scénarios bien ficelés à la difficulté savamment dosée, une équipe aux petits soins';
 $lang['meta_keywords_content'] = 'escape room, Vevey, escape game, Rivievra, Lavaux, activité, divertissement';
 $lang['how_it_works'] = 'Concept';
 $lang['what_public'] = 'Quel public';
