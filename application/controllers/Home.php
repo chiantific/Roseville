c761b23b5ebc12db3787136fe5ab35613965f72d
dunatotatos <contact@dunatotatos.com>
2017-08-09 22:13:04 +0300

    Include index.php in CI

---
+++ application/controllers/Home.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Home Controller
+ *
+ * @package Controllers
+ */
+class Home extends CI_Controller {
+	/**
+	 * Class Constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->load->library('session');
+		// Set user's selected language.
+		if ($this->session->userdata('language')) {
+			$this->config->set_item('language', $this->session->userdata('language'));
+			$this->lang->load('translations', $this->session->userdata('language'));
+		} else {
+			$this->lang->load('translations', $this->config->item('language')); // default
+		}
+	}
+
+    public function index() {
+        $view['base_url'] = $this->config->item('base_url');
+        $this->load->view('home', $view);
+    }
+}
+
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
