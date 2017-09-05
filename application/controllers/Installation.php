083893dfe9aa3732f4e09a57dcf3851e6391401c
dunatotatos <contact@dunatotatos.com>
2017-09-05 12:50:47 +0300

    Update installation process

--- application/controllers/Installation.php
+++ application/controllers/Installation.php
@@ -1,16 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/* ----------------------------------------------------------------------------
- * Easy!Appointments - Open Source Web Scheduler
- *
- * @package     EasyAppointments
- * @author      A.Tselegidis <alextselegidis@gmail.com>
- * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
- * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
- * @link        http://easyappointments.org
- * @since       v1.1.0
- * ---------------------------------------------------------------------------- */
-
 /**
  * Installation Controller
  *
