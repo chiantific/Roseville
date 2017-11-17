dca0a85ceb92e537a5536becbc22fa97c3188da4
dunatotatos <contact@dunatotatos.com>
2017-11-17 16:24:59 +0300

    Add right button to carousel

--- application/views/home.php
+++ application/views/home.php
@@ -392,6 +392,10 @@
                                                 <!-- Left and right controls -->
                                                 <a class="left carousel-control" href="#IVVcarousel" data-slide="prev">
                                                     <span class="glyphicon glyphicon-chevron-left"></span>
+                                                    <span class="sr-only">Previous</span>
+                                                </a>
+                                                <a class="right carousel-control" href="#IVVcarousel" data-slide="next">
+                                                    <span class="glyphicon glyphicon-chevron-right"></span>
                                                     <span class="sr-only">Next</span>
                                                 </a>
                                             </div> <!-- Carousel -->
