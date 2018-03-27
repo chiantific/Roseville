adb352fadc64e1320f2014709c4d4abade98e8ee
dunatotatos <contact@dunatotatos.com>
2018-03-27 22:26:52 +0300

    Disable obsolete persistent DB connnection

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => Config::DB_NAME,
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => TRUE,
+	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
